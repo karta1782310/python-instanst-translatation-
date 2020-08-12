9ace93b3863acaf3007404a77b3caf921b737cbe
Yu-Cheng, Cheng <karta1782310@gmail.com>
2020-08-13 01:59:06 +0800

    complete the trans func with start and stop

--- main.py
+++ main.py
@@ -4,25 +4,33 @@ import pyperclip
 import tkinter as tk
 from googletrans import Translator
 
-def start_translate(result_label):
+def start_translate(result_label, stop_event):
     btn_start['state'] = tk.DISABLED
+    btn_stop['state'] = tk.ACTIVE
+    stop_event.clear()
     recent_text = ""
+
     while True:
         tmp_text = pyperclip.paste()
-        try:
-            if tmp_text != recent_text:
-                print(tmp_text)
-                recent_text = tmp_text
-                result_text = translator.translate(recent_text, dest='zh-tw').text
-                result_label.configure(text=result_text, justify='left', font=("Helvetica",11), wraplength=380) 
-            time.sleep(0.1)
-        except:
+        if tmp_text != recent_text:
+            print(tmp_text)
+            recent_text = tmp_text
+            result_text = translator.translate(recent_text, dest='zh-tw').text
+            result_label.configure(text=result_text, justify='left', font=("Helvetica",11), wraplength=380) 
+        time.sleep(0.1)
+
+        if stop_event.is_set():
             break
 
 def thread_it(func, *args):
     t = threading.Thread(target=func, args=args)
     t.setDaemon(True)     
     t.start()
+
+def stop_trans():
+    stop_event.set()
+    btn_start['state'] = tk.ACTIVE
+    btn_stop['state'] = tk.DISABLED
 
 if __name__ == "__main__":
     translator = Translator()
@@ -40,8 +48,14 @@ if __name__ == "__main__":
     label_trans = tk.Label(window, text='Translation')
     label_trans.pack()
 
-    btn_start = tk.Button(frame_bottom, text='開始翻譯', command=lambda: thread_it(start_translate, label_trans))
-    btn_start.pack(side=tk.BOTTOM)
+    stop_event = threading.Event()
+    
+    btn_start = tk.Button(frame_bottom, text='開始翻譯', command=lambda: thread_it(start_translate, label_trans, stop_event))
+    btn_start.pack(side=tk.LEFT)
+
+    btn_stop = tk.Button(frame_bottom, text="停止翻譯", command=lambda: stop_trans())
+    btn_stop.pack(side=tk.LEFT)
+    btn_stop['state'] = tk.DISABLED
 
     window.mainloop()
     
